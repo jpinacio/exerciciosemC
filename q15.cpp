3388419b43eba24852dc9c5d232b4a06d7196d16
João Pedro Araújo Inácio <107285384+jpinacio@users.noreply.github.com>
2023-03-28 15:18:11 -0300

    Delete q15.cpp

--- q15.cpp
+++
@@ -1,8 +0,0 @@
-//15) Faça um programa que mostre todos os números pares existentes entre 1 e 50.
-
-#include <stdio.h>
-main () {
-    for (int i=2; i<=50; i+=2) {
-        printf ("%d\n",i);
-    }
-}    
